eab43f8bf8255b597aae21a24b7394ea7e3f8bac
Sebastian Syska <syska.seb@gmail.com>
2021-10-24 02:29:15 +0200

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,12 +5,9 @@ docker-compose down
 docker rmi syskaseb/aimachine-backend
 docker rmi syskaseb/aimachine-frontend
 docker rmi syskaseb/aimachine-ai
+docker rmi nginx:mainline-alpine
 docker volume rm $(docker volume ls -q)
 
 docker system prune --force
-
-docker pull syskaseb/aimachine-backend
-docker pull syskaseb/aimachine-frontend
-docker pull syskaseb/aimachine-ai
 
 docker-compose up -d
